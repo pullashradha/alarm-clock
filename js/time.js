512cccd2eb182a2c0736e17253679c1445f574c1
Sami Al-Jamal and Shradha Pulla <sami+shradha@>
2016-08-02 15:10:18 -0700

    Remove console.log from time.js

--- js/time.js
+++ js/time.js
@@ -3,16 +3,12 @@ function Alarm (alarm) {
 }
 
 Alarm.prototype.getAlarm = function () {
-  console.log(this.alarm);
-  console.log(this.time);
   var self = this;
   var myAlarm = setInterval(function() {
     if (moment().format('HH:mm') == self.alarm) {
       alert("It's time");
     }
-    console.log(moment());
-    console.log(self.alarm);
-  }, 1000);
+  }, 100000);
   return myAlarm;
 };
 
