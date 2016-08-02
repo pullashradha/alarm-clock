8c50479f37a3c5ccd412deec81de65c94775102e
Sami Al-Jamal and Shradha Pulla <sami+shradha@>
2016-08-02 09:52:19 -0700

    Remove time.js code, add watch to gulp serve

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,5 +1,3 @@
-var Time = require('./../js/time.js').TimeModule;
-
 $(document).ready(function() {
   $("#time").text(moment());
 });
--- js/time.js
+++ js/time.js
@@ -1,6 +1,1 @@
-function Time(hour, minute){
-  this.hour = hour;
-  this.minute = minute;
-}
 
-exports.TimeModule = Time;
